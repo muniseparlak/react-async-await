e9a037756e9b1b24a3579b3c7bb1ecc8081c132c
Munise Parlak <diyetisyenmunise@gmail.com>
2023-05-31 00:15:15 +0300

    dosyalar eklendi

---
+++ app.js
@@ -0,0 +1,11 @@
+import axios from "axios"
+
+async function getData(number){
+    const {data : user } =  await axios ("https://jsonplaceholder.typicode.com/users/" + number)
+    const {data : post } =  await axios ("https://jsonplaceholder.typicode.com/posts/" + number)
+
+    console.log("user: ", user);
+    console.log("post: ", post);
+}
+
+export default getData;
---
+++ index.js
@@ -0,0 +1,4 @@
+import getData from "./app.js";
+
+const data = getData(1)
+console.log(data);
